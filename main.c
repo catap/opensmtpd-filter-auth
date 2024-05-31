801e13dfab0e60304cf2cd5935da0ea1e2e4811f
Kirill A. Korinsky <kirill@korins.ky>
2024-06-01 00:05:37 +0100

    prevent dkim_ar_cat from lose last character
    It losed the last character when the length resulted string was multiple
    of 256

--- main.c
+++ main.c
@@ -1740,7 +1740,7 @@ dkim_ar_cat(char **ar, size_t *n, size_t aroff, const char *fmt, ...)
 	va_end(ap);
 	if (size <= 0)
 		return -1;
-	if (size + aroff <= *n)
+	if (size + aroff < *n)
 		return (ssize_t)size + aroff;
 	nn = (((aroff + size)  / 256) + 1) * 256;
 	artmp = realloc(*ar, nn);
