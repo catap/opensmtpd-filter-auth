0aab35fd771a8dae9e8dd7de830c3306c40bb22f
Kirill A. Korinsky <kirill@korins.ky>
2024-09-19 22:50:22 +0200

    Do not treat ar_h_errno == NO_DATA as error
    OpenBSD's asr returns ar_h_errno == NO_DATA when it had a fallback from
    UDP to TCP, so I should tolerate it as well.

--- main.c
+++ main.c
@@ -2353,9 +2353,7 @@ spf_resolve(struct asr_result *ar, void *arg)
 		goto end;
 	}
 
-	if (ar->ar_h_errno == HOST_NOT_FOUND
-		|| ar->ar_h_errno == NO_DATA
-		|| ar->ar_h_errno == NO_ADDRESS) {
+	if (ar->ar_h_errno == HOST_NOT_FOUND) {
 		if (query->include && !query->exists)
 			spf_done(query->spf,
 				AR_PERMERROR, hstrerror(ar->ar_h_errno));
